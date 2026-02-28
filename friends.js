b4d4a300c1745fe511563ef274cebfbc123fdf1a
phillister35 <philippe.truong.dang@gmail.com>
2026-02-28 21:33:57 +0700

    friend changes

--- friends.js
+++ friends.js
@@ -1,4 +1,9 @@
 let friends = ["John", "Paul", "Sean"];
 friends.push("Howard");
 console.log(friends.length);
-console.log(friends[0]);
+console.log(friends[0]);friends.unshift("May")
+friends.pop()
+friends.splice(1, 1, "Ringo");
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+}
