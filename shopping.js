d2336230b371f02fb51cd7c8eb8b0151020ee2cd
phillister35 <philippe.truong.dang@gmail.com>
2026-02-28 20:20:12 +0700

    shopping time

---
+++ shopping.js
@@ -0,0 +1,4 @@
+const cart = ['Bánh mì', 'Sữa', 'Trứng', 'Táo', 'Gạo', 'Cà phê', 'Đường', 'Muối', 'Dầu ăn', 'Nước mắm'];
+console.log('Tổng số món hàng trong giỏ:', cart.length);
+console.log('Món hàng thêm vào đầu tiên:', cart[0]);
+console.log('Món hàng thêm vào sau cùng:', cart[cart.length - 1]);
